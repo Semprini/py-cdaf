0a71e97c47196d61106ef99ebb702556d2228739
Paul <Semprini@users.noreply.github.com>
2019-04-24 18:01:44 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,11 @@ from distutils.core import setup
 if os.name == 'nt':
 	# We're on Windows
     url = 'https://github.com/cdaf/windows/archive/master.zip'
+    emulate_script  'cdEmulate.bat'
 else:
 	# We're on Linux
-	url = 'https://github.com/cdaf/linux/archive/master.zip'
+    url = 'https://github.com/cdaf/linux/archive/master.zip'
+    emulate_script  'cdEmulate.sh'
 
 prefix = 'windows-master/'
 response, header = urllib.request.urlretrieve(url)
@@ -23,6 +25,7 @@ with zipfile.ZipFile(response) as zip:
 scripts = sysconfig.get_path('scripts')
 shutil.rmtree(scripts+'/automation', ignore_errors=True)
 shutil.move('build/'+prefix+'automation', scripts+'/automation')
+shutil.move(scripts+'/automation/'+emulate_script, scripts+'/'+emulate_script)
 
 setup(name='pycdaf',
     version='0.1-alpha',
